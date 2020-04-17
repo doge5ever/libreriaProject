23227edd5b4ed339a21a6e127d10513d65a17f43
Dominic Awa <dominic_awa@yahoo.com>
2020-04-17 13:12:57 -0700

    Change directory for book_covers

--- public/src/app/landing/landing.component.ts
+++ public/src/app/landing/landing.component.ts
@@ -7,23 +7,23 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  randomBooks; 
+  private ROOT_URL = "http://localhost:8000/";
+  booksYouMightLike; 
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    
+    this.getRandomPicks();
   }
-  private ROOT_URL = "http://localhost:8000/";
+
 
   getRandomPicks = () => {
     let params = new HttpParams()
       .set('random', 'true')
-      .set('length', '5')
-      .set('select', 'title')
-      .append('select', 'UPC');
-    this.randomBooks = this.http.get(this.ROOT_URL + 'api/books', {params: params}).subscribe((data) => {
-      console.log(params)
-      console.log(data)
+      .set('length', '5');
+
+    this.http.get(this.ROOT_URL + 'api/books', {params: params}).subscribe((data) => {
+      this.booksYouMightLike = data;
     })
   }
 }
