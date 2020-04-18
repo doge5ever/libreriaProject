b11f40bbec381e6be7a2bab62c3ee568e00a945c
Dominic Awa <dominic_awa@yahoo.com>
2020-04-18 01:18:00 -0700

    Added pagination

--- public/src/app/search/search.component.ts
+++ public/src/app/search/search.component.ts
@@ -14,8 +14,10 @@ export class SearchComponent implements OnInit {
   keywords: string;
   searchResults: Object;
   noOfResults;
-  page;
+  pageNumber;
   totalPages;
+  readonly noOfIndices = 7;
+  pageIndices;
   greaterThanValue;
   lessThanValue;
   inBetweenValue1;
@@ -28,20 +30,14 @@ export class SearchComponent implements OnInit {
     private http: HttpClient
     ) { }
 
-  ngOnInit(): void {    
-
-    this.route.params
-      .subscribe(params => {
-        this.keywords = params.keywords;
-        this.page = params.page;
-    });
-
-    let params = new HttpParams()
+    readonly queryDatabase = () => {
+      let params = new HttpParams()
       .set('keywords', this.keywords)
       .set('limit', this.resultsPerPage)
-      .set('page', this.page? this.page : '1')
+      .set('page', this.pageNumber? this.pageNumber : '1')
       .set('select', 'title')
-      .set('select', 'UPC')
+      .append('select', 'product_id')
+      .append('select', 'UPC')
       .append('select','rating')
       .append('select', 'price_USD')
       .append('select', 'availability')
@@ -49,10 +45,38 @@ export class SearchComponent implements OnInit {
     this.http.get(this.ROOT_URL + 'api/books', {params: params}).subscribe((results) => {
       this.searchResults = results['docs'];
       this.noOfResults = results['total'];
-      this.page = results['page'];
+      this.pageNumber = results['page'];
       this.totalPages = results['pages'];
-    })
-    
+      this.pageIndices = this.generateIndices(this.pageNumber, this.totalPages, this.noOfIndices);
+      })
+    }
+
+    readonly generateIndices = (pageNumber, totalPages, noOfIndices) => {
+      let wingSize = (noOfIndices-1)/2;
+      if (pageNumber <= wingSize) {
+        return [...Array(noOfIndices).keys()].map(elt => elt+1);
+      } else if (pageNumber >= totalPages - wingSize) {
+        return [...Array(noOfIndices).keys()].map(elt => elt+totalPages-noOfIndices+1);
+      } else {
+        return [...Array(noOfIndices).keys()].map(elt => elt+pageNumber-wingSize+1)
+      }
+    };
+
+  ngOnInit(): void {    
+
+    this.route.params
+      .subscribe(params => {
+        this.keywords = params.keywords;
+        this.queryDatabase();
+        window.scroll(0,0)
+    });
+    this.route.queryParams
+      .subscribe(params => {
+        this.pageNumber = params.page;
+        console.log(this.pageNumber)
+        this.queryDatabase();
+        window.scroll(0,0)
+    });
   }
 
   clearField = (option) => {
@@ -92,4 +116,7 @@ export class SearchComponent implements OnInit {
     this.router.navigate(['/search', this.keywords], {queryParams: {val1: val1, val2: val2}});
     console.log("Filter values sent- val1:", val1, ", val2:", val2)
   }
+
+  readonly trackByPageIndices = (pageNumber) => {return pageNumber}
+
 }
