4a36d74c1d540add3dec1e91b43f2e7fffd217d8
Dominic Awa <dominic_awa@yahoo.com>
2020-04-22 13:42:21 -0700

    Routed the checkout

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { SearchComponent } from './search/search.component';
 import { BookComponent } from './book/book.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'search', component: SearchComponent},
   {path: 'book/:product_id', component: BookComponent},
   {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent},
   {path: '**', component: PagenotfoundComponent }
   ];
 
