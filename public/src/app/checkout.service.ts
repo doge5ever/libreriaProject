7534b6e6af75bc8d47c37454bf31be0998092b35
Dominic Awa <dominic_awa@yahoo.com>
2020-04-23 22:10:46 -0700

    Added checkout object

--- public/src/app/checkout.service.ts
+++ public/src/app/checkout.service.ts
@@ -4,6 +4,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CheckoutService {
+  checkout: {
+    contactDetails: {
+      firstName: String,
+      lastName: String,
+      emailAddress: String,
+      phoneNumber: Number
+    }, 
+  shippingAddress: {
+    streetAddress: String,
+    city: String,
+    state: String,
+    zipCode: Number,
+    country: String
+    },
+  paymentMethod: {
+    nameOnCard: String,
+    creditCardNumber: Number,
+    ExpMonth: Number,
+    ExpYear: Number,
+    CVV: Number
+    },
+  billingAddress: {
+    isSameAddress: Boolean,
+    streetAddress: String,
+    city: String,
+    state: String,
+    zipCode: Number,
+    country: String
+    },
+  }
 
   constructor() { }
+
+  updateForm = (form) => {
+    
+  }
 }
