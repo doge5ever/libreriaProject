48846280c63b21fca331b14366503293ba60156c
Dominic Awa <dominic_awa@yahoo.com>
2020-04-24 17:45:55 -0700

    Connected checkout component and service

--- public/src/app/checkout-shipping-and-payment/checkout-shipping-and-payment.component.ts
+++ public/src/app/checkout-shipping-and-payment/checkout-shipping-and-payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'checkout-app-shipping-and-payment',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-shipping-and-payment.component.scss']
 })
 export class CheckoutShippingAndPaymentComponent implements OnInit {
+  checkoutForm: Object;
 
-  constructor() { }
+  constructor(
+    private checkoutService: CheckoutService
+  ) {
+    this.checkoutForm = {
+      contactDetails: {
+        firstName: ''
+      },
+      shippingAddress: {},
+      paymentMethod: {
+        billingAddress: {}
+      }
+    }
+  };
 
   ngOnInit(): void {
   }
 
+  submitForm = (form): void => {
+    this.checkoutService.updateForm(form.value);
+  }
 }
--- public/src/app/checkout.service.ts
+++ public/src/app/checkout.service.ts
@@ -1,43 +1,86 @@
 import { Injectable } from '@angular/core';
 
+interface checkoutInterface {
+  contactDetails: {
+      firstName: string,
+      lastName: string,
+      emailAddress: string,
+      phoneNumber?: number
+    }, 
+  shippingAddress: {
+    streetAddress: string,
+    city: string,
+    state: string,
+    zipCode: number,
+    country: string
+    },
+  paymentMethod: {
+    nameOnCard: string,
+    creditCardNumber: number,
+    expMonth: number,
+    expYear: number,
+    CVV: number,
+    billingAddress: {
+      isSameAddress: boolean,
+      streetAddress: string,
+      city: string,
+      state: string,
+      zipCode: number,
+      country: string
+    },  
+  },
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
-  checkout: {
-    contactDetails: {
-      firstName: String,
-      lastName: String,
-      emailAddress: String,
-      phoneNumber: Number
-    }, 
-  shippingAddress: {
-    streetAddress: String,
-    city: String,
-    state: String,
-    zipCode: Number,
-    country: String
-    },
-  paymentMethod: {
-    nameOnCard: String,
-    creditCardNumber: Number,
-    ExpMonth: Number,
-    ExpYear: Number,
-    CVV: Number
-    },
-  billingAddress: {
-    isSameAddress: Boolean,
-    streetAddress: String,
-    city: String,
-    state: String,
-    zipCode: Number,
-    country: String
-    },
+  checkoutForm: checkoutInterface;
+
+  constructor() {
+    this.checkoutForm = {
+      contactDetails: {
+        firstName: undefined,
+        lastName: undefined,
+        emailAddress: undefined,
+        phoneNumber: undefined
+      }, 
+      shippingAddress: {
+        streetAddress: undefined,
+        city: undefined,
+        state: undefined,
+        zipCode: undefined,
+        country: undefined
+        },
+      paymentMethod: {
+        nameOnCard: undefined,
+        creditCardNumber: undefined,
+        expMonth: undefined,
+        expYear: undefined,
+        CVV: undefined,
+        billingAddress: {
+          isSameAddress: undefined,
+          streetAddress: undefined,
+          city: undefined,
+          state: undefined,
+          zipCode: undefined,
+          country: undefined
+        },  
+      },
+
+    }
   }
 
-  constructor() { }
+  updateForm = (form: checkoutInterface):void => {
+    console.log("this is the form submitted", form)
+    Object.keys(form).forEach((key) => {
+      this.checkoutForm[key] = form[key];
+      console.log(this.checkoutForm)
+    })
+    console.log("Updated the checkout form: ", this.checkoutForm)
+  }
 
-  updateForm = (form) => {
-    
+  getForm = ():checkoutInterface => {
+    return this.checkoutForm;
   }
 }
