28171b74f8d33b10e551b184d5d550e61b332301
Dominic Awa <dominic_awa@yahoo.com>
2020-04-27 11:23:49 -0700

    Fixed indentation

--- server/models/ordersModel.js
+++ server/models/ordersModel.js
@@ -3,38 +3,38 @@ const mongoosePaginate = require('mongoose-paginate');
 
 module.exports = function() {
     var orderSchema = new mongoose.Schema({
-        contactDetails: {
-            firstName: { type: String, required: [true, 'First Name is required.'] },
-            lastName: { type: String, required: [true, 'Last Name is required.'] },
-            emailAddress: { type: String, required: [true, 'Email Address is required.'] },
-            phoneNumber: { type: String },
-        },
-        shippingAddress: {
-            streetAddress: { type: String, required: [ true, 'Street Address is required.'] },
-            city: { type: String, required: [true, 'City is required.']},
-            state: { type: String, required: [true, 'State is required.']},
-            zipCode: { type: String, required: [true, 'Zip Code is required.']},
-            country: { type: String, required: [true, 'Country is required.']},
-        },
-        paymentMethod: {
-            nameOnCard: { type: String, required: [true, 'Name on Card is required.'] },
-            creditCardNumber: { type: Number, required: [true, 'Credit Card Number is required.'] },
-            expMonth: { type: String, required: [true, 'Exp. Month is required.'] },
-            expYear: { type: String, required: [true, 'Exp. Year is required.'] },
-            CVV: { type: String, required: [true, 'CVV is required.'] },
-            billingAddress: {
-                streetAddress: { type: String, required: [ true, 'Street Address is required.'] },
-                city: { type: String, required: [true, 'City is required.']},
-                state: { type: String, required: [true, 'State is required.']},
-                zipCode: { type: String, required: [true, 'Zip Code is required.']},
-                country: { type: String, required: [true, 'Country is required.']},
-            }
+      contactDetails: {
+        firstName: { type: String, required: [true, 'First Name is required.'] },
+        lastName: { type: String, required: [true, 'Last Name is required.'] },
+        emailAddress: { type: String, required: [true, 'Email Address is required.'] },
+        phoneNumber: { type: String },
+      },
+      shippingAddress: {
+        streetAddress: { type: String, required: [ true, 'Street Address is required.'] },
+        city: { type: String, required: [true, 'City is required.']},
+        state: { type: String, required: [true, 'State is required.']},
+        zipCode: { type: String, required: [true, 'Zip Code is required.']},
+        country: { type: String, required: [true, 'Country is required.']},
+      },
+      paymentMethod: {
+        nameOnCard: { type: String, required: [true, 'Name on Card is required.'] },
+        creditCardNumber: { type: Number, required: [true, 'Credit Card Number is required.'] },
+        expMonth: { type: String, required: [true, 'Exp. Month is required.'] },
+        expYear: { type: String, required: [true, 'Exp. Year is required.'] },
+        CVV: { type: String, required: [true, 'CVV is required.'] },
+        billingAddress: {
+          streetAddress: { type: String, required: [ true, 'Street Address is required.'] },
+          city: { type: String, required: [true, 'City is required.']},
+          state: { type: String, required: [true, 'State is required.']},
+          zipCode: { type: String, required: [true, 'Zip Code is required.']},
+          country: { type: String, required: [true, 'Country is required.']},
         }
+      }
     }, 
     {
-        timestamps: true 
+      timestamps: true 
     });
-    
+
     orderSchema.plugin(mongoosePaginate);
     mongoose.model('Order', orderSchema);
 }
