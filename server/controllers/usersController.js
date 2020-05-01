6dda5a3cd29c0bbc38cbe9c1ecd5254523f65ef3
Dominic Awa <dominic_awa@yahoo.com>
2020-04-30 23:04:47 -0700

    Login and registration functional

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,11 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+  bcrypt = require('bcrypt');
+
 const User = mongoose.model('User');
+const saltRounds = 10;
 
 module.exports = {
   registerUser: (req, res) => {
     console.log("I will be registering the user.")
+    bcrypt.hash(req.body.password, saltRounds)
+      .then((output) => {
+        document = new User({
+          emailAddress: req.body.emailAddress,
+          hash: output
+        })
+        document.save((err, doc) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Document is saved as following: ", doc)
+          }
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }, 
   authenticateUser: (req, res) => {
-    console.log("I will be aunthenticating the user.")
+    User
+      .findOne({emailAddress: req.body.emailAddress})
+      .then((document) => {
+        bcrypt.compare(req.body.password, document.hash)
+          .then((bool) => {
+            console.log('Here is the result for the comparison test:', bool);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 };
--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -3,9 +3,8 @@ const mongoosePaginate = require('mongoose-paginate');
 
 module.exports = function() {
     var userSchema = new mongoose.Schema({
-      username: { type: String, required: [true, 'Username is required.']},
+      emailAddress: { type: String, required: [true, 'Email is required.']},
       hash: { type: String, required: [true, 'Hash is required.']},
-      salt: { type: String, required: [true, 'Salt is required.']}
     },
     {
       timestamps: true 
