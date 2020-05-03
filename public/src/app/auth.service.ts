499d151add159a0d36c8fd33d1183cd340ca6e2a
Dominic Awa <dominic_awa@yahoo.com>
2020-05-03 13:35:47 -0700

    Minor

--- public/src/app/auth.service.ts
+++ public/src/app/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isAuthenticated: boolean;
 
-  constructor() { }
+  constructor(
+    private http: HttpService
+  ) { 
+    this.isAuthenticated = false;
+  }
+
+  getUserInfo = (): boolean => {
+    let userData = localStorage.getItem('userInfo');
+    if (userData && JSON.parse(userData)) {return true;}
+    return false;
+  }
+  
+  setUserInfo = (user): void => {
+    localStorage.setItem('userInfo', JSON.stringify(user));
+  }
+
+
 }
