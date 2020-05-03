d60178dfdf9e26621e88b39ad47437c36eedc67c
Dominic Awa <dominic_awa@yahoo.com>
2020-05-03 00:16:33 -0700

    Defined login and guest form controls

--- public/src/app/checkout-login/checkout-login.component.ts
+++ public/src/app/checkout-login/checkout-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-login',
@@ -6,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-login.component.scss']
 })
 export class CheckoutLogInComponent implements OnInit {
-  logInFormControl
+  loginFormControl: FormGroup;
+  guestFormControl: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginFormControl = new FormGroup({
+      emailAddress: new FormControl(''),
+      password: new FormControl('')
+    });
 
+    this.guestFormControl = new FormGroup({
+      emailAddress: new FormControl('', Validators.email),
+      confirmEmailAddress: new FormControl('')
+    });
   }
 
+  loginOnSubmit = () => {
+
+  };
+
+  guestOnSubmit = () => {
+
+  };
 }
