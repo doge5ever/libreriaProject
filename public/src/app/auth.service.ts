a58561aa8c5e29a1bd18b38b66bb1f5955558848
Dominic Awa <dominic_awa@yahoo.com>
2020-05-03 17:44:23 -0700

    Added firstName to FE to retrieve after login

--- public/src/app/auth.service.ts
+++ public/src/app/auth.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   isAuthenticated: boolean;
+  firstName: string;
 
   constructor(
     private http: HttpService
   ) { 
     this.isAuthenticated = false;
+    this.firstName = '';
   }
   
   updateAuthentication = (res) => {
     if (res.valid) {
       this.isAuthenticated = true;
+      this.firstName = res.firstName;
+      console.log(res)
       console.log('Value for isAuthenticated: ', this.isAuthenticated);
+      console.log('FirstName: ', this.firstName);
     }
   }  
 }
