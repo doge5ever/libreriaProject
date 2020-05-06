0154ec3e65e38b49e44dd52314d8188f302aa6ef
Dominic Awa <dominic_awa@yahoo.com>
2020-05-06 16:18:27 -0700

    Bookstore to Libreria

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('./server/config/mongoose')();
 
 // For DEVELOPMENT PURPOSE. Delete when being deployed.
 app.use(cors());
-app.use(express.static( __dirname + '/public/dist/bookstoreProject' ));
+app.use(express.static( __dirname + '/public/dist/libreriaProject' ));
 app.use(bodyParser.json());
 
 require('./server/config/passport')(app);
